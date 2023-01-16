bb57a19248bf76c57cf9b2d33b09860295a959f4
Richard Lu <richardblu37@gmail.com>
2023-01-16 15:49:46 -0500

    update home page styles and componentize in Home.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
 import React, { useState, useEffect, createContext, useContext } from 'react'
 import './App.css'
-import Habit, { HabitNode } from './components/Habit'
-import styled from 'styled-components'
-import CreateHabitModal from './components/CreateHabitModal'
+import { HabitNode } from './components/Habit'
 import { tasks } from './sampleData'
+import Home from './components/Home'
 
 /* State context */
 interface HabitState {
@@ -21,96 +20,15 @@ export function useHabitState() {
 }
 
 export default function App() {
-  const [createModalIsVisible, setCreateModalIsVisible] = useState(false)
-
   const [habits, setHabits] = useState<HabitNode[]>(tasks)
 
   // Current date stored at the top level
   const today = new Date()
   const [date, setDate] = useState<Date>(today)
-
+  
   return (
     <HabitStateContext.Provider value={{ habits, setHabits, date, setDate }}>
-      <div className="App" >
-        <button type='button' onClick={() => setCreateModalIsVisible(true)}>
-          Create new habit
-        </button>
-        <Header />
-        <Section title="Today" timeframe="day" />
-        <Section title="Weekly" timeframe="week" />
-        <Section title="Monthly" timeframe="month" />
-        <Section title="Yearly" timeframe="year" />
-        {
-          createModalIsVisible &&
-          <CreateHabitModal
-            close={() => setCreateModalIsVisible(false)}
-          />
-        }
-      </div>
+      <Home />
     </HabitStateContext.Provider>
   )
 }
-
-const Header = () => {
-  const { date, setDate } = useHabitState()
-  return (
-    <header>
-      <DateHeader>
-        {date.toLocaleDateString("en-US", { dateStyle: "full" })}
-      </DateHeader>
-      <button onClick={() => setDate(new Date(date.getFullYear(), date.getMonth(), date.getDate() - 1))}>Previous</button>
-      <button onClick={() => setDate(new Date(date.getFullYear(), date.getMonth(), date.getDate() + 1))}>Next</button>
-    </header>
-  )
-}
-
-const DateHeader = styled.h2`
-  font-weight: 700;
-  font-size: ${19 / 16}rem;
-  text-align: center;
-
-  margin-left: auto;
-  margin-right: auto;
-`
-
-interface SectionProps {
-  title: string,
-  timeframe: string
-}
-
-const Section = ({ title, timeframe }: SectionProps) => {
-  const { habits, date } = useHabitState()
-
-  const sectionHabits = habits.filter(
-    item => item.timeframe === timeframe
-  ).map(
-    item => {
-      // Apply count from current date
-      // Look for current date in item.dates
-      const currentDate = item.progress.dates.find(result => result.date.getDate() === date.getDate())
-      const initialValue = currentDate?.count
-      return (
-        <Habit data={item} key={item.name} initialCount={initialValue} />  // initialCount should sync with the `count` of the habit on the current date
-      )
-    }
-  )
-
-  return (
-    <section>
-      <h2>{title}</h2>
-      <SectionHabitListWrapper>
-        {
-          sectionHabits.length > 0
-            ? sectionHabits
-            : "No habits"
-        }
-      </SectionHabitListWrapper>
-    </section>
-  )
-}
-
-const SectionHabitListWrapper = styled.section`
-  display: flex;
-  flex-direction: column;
-  gap: 16px;
-`
---
+++ src/components/Home.tsx
@@ -0,0 +1,155 @@
+import { useState } from "react"
+import styled, { ThemeProvider } from "styled-components"
+import { lightColorTheme } from "../colorTheme"
+import { tasks } from "../sampleData"
+import CreateHabitModal from "./CreateHabitModal"
+import Habit, { HabitNode } from "./Habit"
+import { useHabitState } from "../App"
+import VisuallyHidden from './common/VisuallyHidden.jsx'
+
+export default function Home() {
+    const [createModalIsVisible, setCreateModalIsVisible] = useState(false)
+    
+    return (
+        <ThemeProvider theme={lightColorTheme}>
+          <div className="App" >
+            <CreateHabitButton type='button' onClick={() => setCreateModalIsVisible(true)}>
+              Create habit
+            </CreateHabitButton>
+            <Header />
+            <SectionsWrapper>
+              <Section title="Today" timeframe="day" />
+              <Section title="Weekly" timeframe="week" />
+              <Section title="Monthly" timeframe="month" />
+              <Section title="Yearly" timeframe="year" />
+            </SectionsWrapper>
+            {
+              createModalIsVisible &&
+              <CreateHabitModal
+                close={() => setCreateModalIsVisible(false)}
+              />
+            }
+          </div>
+        </ThemeProvider>
+    )
+  }
+
+  const SectionsWrapper = styled.section`
+  display: flex;
+  flex-direction: column;
+  gap: 32px;
+`
+
+const CreateHabitButton = styled.button`
+  position: fixed;
+  bottom: 32px;
+
+  margin-left: auto;
+  margin-right: auto;
+`
+
+const Header = () => {
+  const { date, setDate } = useHabitState()
+
+  const day = new Intl.DateTimeFormat("en-us", { weekday: "long" }).format(date)
+  const month = new Intl.DateTimeFormat("en-us", { month: "long" }).format(date)
+
+  return (
+    <HeaderWrapper>
+      <DateHeader>
+        {/* {date.toLocaleDateString("en-US", { dateStyle: "full" })} */}
+        {day}
+        <br />
+        {month + " " + date.getDate()}
+        <br />
+        {date.getFullYear()}
+      </DateHeader>
+      <ButtonGroup>
+        <button onClick={() => setDate(new Date(date.getFullYear(), date.getMonth(), date.getDate() - 1))}>
+          <VisuallyHidden>
+            Previous date
+          </VisuallyHidden>
+          &larr;
+        </button>
+        <button onClick={() => setDate(new Date(date.getFullYear(), date.getMonth(), date.getDate() + 1))}>
+          <VisuallyHidden>
+            Next date
+          </VisuallyHidden>
+          &rarr;
+        </button>
+      </ButtonGroup>
+    </HeaderWrapper>
+  )
+}
+
+const HeaderWrapper = styled.header`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+`
+
+const DateHeader = styled.h2`
+  color: ${props => props.theme.textPrimary + "70"};
+
+  font-weight: 300;
+  font-size: 2rem;
+  line-height: 1.25;
+  text-align: end;
+`
+
+const ButtonGroup = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 16px;
+`
+
+interface SectionProps {
+  title: string,
+  timeframe: string
+}
+
+const Section = ({ title, timeframe }: SectionProps) => {
+  const { habits, date } = useHabitState()
+
+  const sectionHabits = habits.filter(
+    item => item.timeframe === timeframe
+  ).map(
+    item => {
+      // Apply count from current date
+      // Look for current date in item.dates
+      const currentDate = item.progress.dates.find(result => result.date.getDate() === date.getDate())
+      const initialValue = currentDate?.count
+      return (
+        <Habit data={item} key={item.name} initialCount={initialValue} />  // initialCount should sync with the `count` of the habit on the current date
+      )
+    }
+  )
+
+  return (
+    <section>
+      <SectionHeading>{title}</SectionHeading>
+      <SectionHabitListWrapper>
+        {
+          sectionHabits.length > 0
+            ? sectionHabits
+            : "No habits"
+        }
+      </SectionHabitListWrapper>
+    </section>
+  )
+}
+
+const SectionHabitListWrapper = styled.section`
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+`
+
+const SectionHeading = styled.h2`
+  color: ${props => props.theme.textPrimary + "80"};
+  text-align: start;
+  text-transform: uppercase;
+  font-size: 0.875rem;
+  font-weight: 400;
+  letter-spacing: 0.04em;
+`
